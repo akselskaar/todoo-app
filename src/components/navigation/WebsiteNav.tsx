dc0c7c3ac321e507a1c7c0dae2820f65cc1b4fb3
akselhskaar <akselhskaar@hotmail.no>
2024-10-29 21:02:31 +0100

    bugfix: Link component from next with button inside causing 'cant find module 'net'' error

--- src/components/navigation/WebsiteNav.tsx
+++ src/components/navigation/WebsiteNav.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { Button } from '@/components'
 import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
 
 const WebsiteNav = () => {
   return (
     <nav className='flex justify-between items-center px-6 py-4 bg-red-200'>
       <h1 className='font-bold text-lg'>Todoo app</h1>
-      <Link href='/api/auth/signin?callbackUrl=/tasks'>
-        <Button>Logg inn</Button>
+      <Link
+        href='/api/auth/signin?callbackUrl=/tasks'
+        className={buttonVariants({ variant: 'default' })}
+      >
+        Logg inn
       </Link>
     </nav>
   )
