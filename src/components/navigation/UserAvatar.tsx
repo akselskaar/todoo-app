07038046709c478c3063b8c845cf1d402f7dabb6
akselhskaar <akselhskaar@hotmail.no>
2024-10-29 19:09:17 +0100

    add: userAvatar component

---
+++ src/components/navigation/UserAvatar.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Link from 'next/link'
+
+type UserAvatarProps = {
+  name?: string | null
+  image?: string | null
+}
+
+const UserAvatar = ({ name, image }: UserAvatarProps) => {
+  // Get initials from name
+  const getInitials = (name: string | undefined | null) => {
+    if (!name) return 'NN'
+    const [firstName, lastName] = name.split(' ')
+    return `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <>
+          <Avatar>
+            {image != undefined && image != null ? (
+              <AvatarImage src={image} />
+            ) : (
+              <AvatarFallback>{getInitials(name)}</AvatarFallback>
+            )}
+          </Avatar>
+          <span className='sr-only'>Profile menu</span>
+        </>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem>
+          <Link href='/api/auth/signout?callbackUrl=/'>Sign out</Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+export default UserAvatar
