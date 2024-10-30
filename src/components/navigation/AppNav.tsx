933216dd2ea7a4897132c3d55f7c8f84c04a4116
akselhskaar <akselhskaar@hotmail.no>
2024-10-30 17:40:24 +0100

    hotfix: css

--- src/components/navigation/AppNav.tsx
+++ src/components/navigation/AppNav.tsx
@@ -7,8 +7,8 @@ const AppNav = async () => {
   const session = await auth()
   console.log('session: ', session)
   return (
-    <nav className='flex justify-between items-center px-6 py-4 bg-pink-200'>
-      <h1>Todoo app</h1>
+    <nav className='flex justify-between items-center px-6 py-4 bg-primary-a3 border-b border-primary-a6'>
+      <h1 className='text-lg font-bold'>Todoo app</h1>
       <UserAvatar name={session?.user?.name} image={session?.user?.image} />
     </nav>
   )
